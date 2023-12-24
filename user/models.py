e13e38514caec52d220d52cefda60fca62644abb
Gökhan Bıyıkoğlu <gokhan_byk@icloud.com>
2023-12-24 11:42:06 +0300

    kullanıcıya özel profile modeli oluşturuldu

--- user/models.py
+++ user/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from autoslug import AutoSlugField
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Profile(models.Model):
+    name = models.CharField(max_length=20)
+    image = models.FileField(upload_to='profile_picture')
+    slug = AutoSlugField(populate_from = "name")
+
+    user = models.ForeignKey(User, on_delete=models.CASCADE, null=True)
+
+
+    def __str__(self):
+        return self.name
